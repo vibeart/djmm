6041c5cc3533079c049122009be2064dffbb76c5
Leonardorramos <leonardorramos@gmail.com>
2021-09-16 18:53:22 -0300

    implemented layoutof footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,36 @@
+import { Facebook, Instagram } from "../icons"
+import Link from "next/link"
+
 export default function Footer() {
-	return <h1>Footer</h1>
+	return (
+		<>
+			<footer>
+				<div className={`bg-black w-full py-3  text-white`}>
+					<div className=' container flex justify-between items-center '>
+						<div className={`flex space-x-2 `}>
+							<Link href='http://dropbox.com'>
+								<a className={`m-0`}>PRESSKIT</a>
+							</Link>
+
+							<Link href='http://dropbox.com'>
+								<a className={`m-0`}>CONTATO</a>
+							</Link>
+						</div>
+						<div className={`flex space-x-2 `}>
+							<Link href='http://www.instagram.com'>
+								<a>
+									<Instagram />
+								</a>
+							</Link>
+							<Link href='http://www.facebook.com'>
+								<a>
+									<Facebook />
+								</a>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+				"2xl": "6rem",
+			},
+		},
+
 		extend: {},
 	},
 	variants: {
